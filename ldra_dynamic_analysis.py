db5261dc435770e73e8266e0149a3f650eea00eb
mehsan123 <mehsan123@users.noreply.github.com>
2022-06-17 01:13:32 -0400

    Update ldra_dynamic_analysis.py

--- ldra_dynamic_analysis.py
+++ ldra_dynamic_analysis.py
@@ -92,7 +92,7 @@ def main():
     #dynamicJsonFormatter(tcfName)
     #xmltojson (WORKAREA+'Coverage.xml',WORKAREA+'coverage.json')
     xmltojson (workarearoot+tcfName[-17:-4]+"_dyn.xml",workarearoot+tcfName[-17:-4]+'_dyn.json')
-    #junit_creator(tcfName)
+    junit_creator(tcfName)
     #junit_creator()
     
     
@@ -176,7 +176,7 @@ def runanalysis(tcfName):
     command1=sourceRoot+'Clean.bat '+sourceRoot
     os.system(command1)
     
-    command2 = '{}contestbed.exe {}{} /112a340212panq  -create_set=SYSTEM -reanalyse_changed_set -generate_code_review=HTML -tb_workfiledir={} -build_cmd="Build.bat" -startin_dir={} -auto_macro -auto_macro_value="0" -exhdir={}'.format(toolsuiteroot,sourceRoot,tcfName, workarearoot, sourceRoot, projectDir) #pass ptf as parameter
+    command2 = '{}contestbed.exe {}{} /112a340212panq  -create_set=system -reanalyse_changed_set -generate_code_review=HTML -tb_workfiledir={} -build_cmd="Build.bat" -startin_dir={} -auto_macro -auto_macro_value="0" -exhdir={}'.format(toolsuiteroot,sourceRoot,tcfName, workarearoot, sourceRoot, projectDir) #pass ptf as parameter
     os.system(command2)
     
     command4=sourceRoot+'Build.bat '+sourceRoot 
