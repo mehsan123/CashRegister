9bb42aa0fb73e1cd636349144fefd577d917e865
mehsan123 <mehsan123@users.noreply.github.com>
2022-07-18 14:45:09 -0400

    Add files via upload

---
+++ unitTest.bat
@@ -0,0 +1,10 @@
+set TOOL=%1
+set TCF_NAME=%2
+set PUBLISH=%3
+set SEQ=%4
+set BUILD_DIR=%5
+
+echo hi
+
+%TOOL%\\Contbrun.exe %BUILD_DIR%%TCF_NAME% -seq=%BUILD_DIR%\\LowLevelTests\\%SEQ%.tcf -unit_publish_to=%PUBLISH% -box=black -regress -quit
+ldra_unitTest_junit.py %PUBLISH%\\%SEQ%\\%SEQ%.thr.txt
