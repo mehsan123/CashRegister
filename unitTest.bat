6563ff6dcefea11ca2fce902b03439cee4d260db
mehsan123 <mehsan123@users.noreply.github.com>
2022-07-19 13:18:59 -0400

    Update unitTest.bat

--- unitTest.bat
+++ unitTest.bat
@@ -5,6 +5,8 @@ set SEQ=%4
 set BUILD_DIR=%5
 
 echo hi
+%TOOL%\\contestbed.exe -112a34 %BUILD_DIR%%TCF_NAME%.tcf
 
-%TOOL%\\Contbrun.exe %BUILD_DIR%%TCF_NAME% -seq=%BUILD_DIR%\\LowLevelTests\\%SEQ%.tcf -unit_publish_to=%PUBLISH% -box=black -regress -quit
-ldra_unitTest_junit.py %PUBLISH%\\%SEQ%\\%SEQ%.thr.txt
+%TOOL%\\Contbrun.exe %TCF_NAME%.tcf -tcf=%BUILD_DIR%\\%SEQ%.tcf -unit_publish_to="%PUBLISH%" -box=black -regress -quit
+
+ldra_unitTest_junit.py %PUBLISH%
