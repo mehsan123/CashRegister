67f332b6c48c871c49ba0abe5b09a271f75fbdde
mehsan123 <mehsan123@users.noreply.github.com>
2022-07-19 22:06:41 -0400

    Update unitTest.bat

--- unitTest.bat
+++ unitTest.bat
@@ -5,7 +5,7 @@ set SEQ=%4
 set BUILD_DIR=%5
 
 echo hi
-%TOOL%\\contestbed.exe -112a34 %BUILD_DIR%%TCF_NAME%.tcf
+%TOOL%\\contestbed.exe -112a34  %BUILD_DIR%%TCF_NAME%.tcf -workdir=%BUILD_DIR%
 
 %TOOL%\\Contbrun.exe %TCF_NAME%.tcf -tcf=%BUILD_DIR%\\%SEQ%.tcf -box=white -regress -quit -unit_publish_to="%PUBLISH%" 
 rem %TOOL%\\Contbrun.exe %TCF_NAME%.tcf -tcf="%BUILD_DIR%\%SEQ%.tcf" -unit_publish_to="%PUBLISH%" -box=black -regress -quit 
