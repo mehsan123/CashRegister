f19173348ab3572fc5f77649c006157e2f085f9b
mehsan123 <mehsan123@users.noreply.github.com>
2022-07-21 11:05:40 -0400

    Update unitTest.bat

--- unitTest.bat
+++ unitTest.bat
@@ -6,11 +6,9 @@ set BUILD_DIR=%5
 echo %PUBLISH%
 echo %BUILD_DIR%
 echo hi
-rem %TOOL%\\tbini WORKAREA_BASEDIR=%PUBLISH%
-rem %TOOL%\\tbini PERMDIR=%PUBLISH%
-rem %TOOL%\\contestbed.exe -112a34  %BUILD_DIR%\%TCF_NAME%.tcf " -auto_macro 
-
-%TOOL%\\Contbrun.exe "%TCF_NAME%.tcf" -tcf="%BUILD_DIR%\%SEQ%.tcf" -box=black -regress -quit -unit_publish_to="%PUBLISH%\" 
+\
+%TOOL%\\contestbed.exe -112a34  %BUILD_DIR%%TCF_NAME%.tcf -tb_workfiledir="%PUBLISH%\" 
+%TOOL%\\Contbrun.exe "%TCF_NAME%.tcf" -tcf="%BUILD_DIR%\%SEQ%.tcf" -box=white -regress -quit -unit_publish_to="%PUBLISH%\" 
 
 rem %TOOL%\\Contbrun.exe %TCF_NAME%.tcf -tcf="%BUILD_DIR%\%SEQ%.tcf" -unit_publish_to="%PUBLISH%" -box=black -regress -quit 
 
